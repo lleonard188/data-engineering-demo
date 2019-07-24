99f8125ce108c3a610159d080ed4617635933f12
lleonard188 <lleonard188@gmail.com>
2019-07-24 12:45:48 +0800

    Inputted filepath to fastq

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,7 +2,7 @@ import org.apache.spark.sql.SparkSession
 
 object SimpleApp {
   def main(args: Array[String]) {
-    val logFile = "YOUR_SPARK_HOME/README.md" // Should be some file on your system
+    val logFile = "/home/len/sratoolkit.2.9.2-ubuntu64/SRR3997871_1.fastq" // Should be some file on your system
     val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
     val logData = spark.read.textFile(logFile).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
